5aaf1a06e44638499373b1358c85d1546187edae
laiyurui <45766023+laiyurui@users.noreply.github.com>
2026-05-23 13:11:31 +0800

    Add files via upload

---
+++ main.cmd
@@ -0,0 +1,35 @@
+
+python main.py      --reduction_rate 0.05       --dataset cora  --qwen_model_name qwen3-max   --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 50   
+python main.py      --reduction_rate 0.05       --dataset citeseer  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100  
+python main.py      --reduction_rate 0.05       --dataset dblp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.05             --dataset elecomp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.05  --pdropout 0.4           --dataset elephoto  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 0.05            --dataset bookhis  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 128 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 0.05            --dataset wikics  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100 
+
+python main.py      --reduction_rate 0.25       --dataset cora  --qwen_model_name qwen3-max   --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 50   
+python main.py      --reduction_rate 0.25       --dataset citeseer  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100  
+python main.py      --reduction_rate 0.25       --dataset dblp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.25             --dataset elecomp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.25             --dataset elephoto  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 0.25            --dataset bookhis  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 128 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 0.25            --dataset wikics  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100
+
+
+python main.py      --reduction_rate 0.5       --dataset cora  --qwen_model_name qwen3-max   --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 50   
+python main.py      --reduction_rate 0.5       --dataset citeseer  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100  
+python main.py      --reduction_rate 0.5       --dataset dblp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.5             --dataset elecomp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 0.5             --dataset elephoto  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100    
+ python main.py         --reduction_rate 0.5            --dataset bookhis  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 128 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 0.5            --dataset wikics  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100 
+
+python main.py      --reduction_rate 1.0       --dataset cora  --qwen_model_name qwen3-max   --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 50   
+python main.py      --reduction_rate 1.0       --dataset citeseer  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100  
+python main.py      --reduction_rate 1.0       --dataset dblp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 1.0             --dataset elecomp  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100   
+python main.py         --reduction_rate 1.0             --dataset elephoto  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100    
+ python main.py         --reduction_rate 1.0            --dataset bookhis  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 128 --dnum 3 --cdnum 100    
+python main.py         --reduction_rate 1.0            --dataset wikics  --qwen_model_name qwen3-max  --word_lim 256 --tok_lim 256 --dnum 3 --cdnum 100 
+
+
